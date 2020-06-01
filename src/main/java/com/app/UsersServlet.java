89488f89ba8c87226224422e6af8507bd3826864
Max Belushkin <belushkin@gmail.com>
2020-06-01 21:41:28 +0300

    Added first page

---
+++ src/main/java/com/app/UsersServlet.java
@@ -0,0 +1,51 @@
+package com.app;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.Version;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "usersServlet", urlPatterns = {"/users"})
+public class UsersServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Configuration cfg = new Configuration();
+        cfg.setIncompatibleImprovements(new Version("2.3.23"));
+        cfg.setClassForTemplateLoading(UsersServlet.class, "/");
+        cfg.setDefaultEncoding("UTF-8");
+
+        Template template = cfg.getTemplate("templates/people-list.ftl");
+
+        Map<String, Object> input = new HashMap<>();
+
+        PrintWriter pw = resp.getWriter();
+        try (StringWriter out = new StringWriter()) {
+            template.process(input, out);
+            pw.append(out.getBuffer().toString());
+            out.flush();
+            pw.flush();
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String submit = req.getParameter("submit");
+        String userId = req.getParameter("user_id");
+
+        System.out.println(submit);
+        System.out.println(userId);
+    }
+}
